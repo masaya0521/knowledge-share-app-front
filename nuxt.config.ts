58c4a19aa7800b75cce58e010a12b10de164c8b4
masaya0521 <kaji0521km@gmail.com>
2022-08-22 23:36:13 +0900

    不要なコメント削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,6 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // 公式のチュートリアル通りだと動かない
-  // 対応issue:
   buildModules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/css/tailwind.css"], // <= ドキュメントからさらに追加
 });
