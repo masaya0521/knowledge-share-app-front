778f498c2cfeafb40c0d3f98815daa1ab56152ba
masaya0521 <kaji0521km@gmail.com>
2022-08-23 22:01:29 +0900

    fix lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   // tailwindを適用したいファイル群を指定
   content: [
-    "./app.vue", // <= ドキュメントからさらに追加
-    "./components/**/*.{vue,js}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
+    './app.vue', // <= ドキュメントからさらに追加
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
   ],
   theme: {
     extend: {},
